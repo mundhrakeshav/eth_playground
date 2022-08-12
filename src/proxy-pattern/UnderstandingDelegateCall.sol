71e46b5b36c3b2a39581907e6c8c4085b8b7b859
Keshav Mundhra <mundhrakeshav11@gmail.com>
2022-08-12 20:35:33 +0530

    proxy patterns

--- src/proxy-pattern/UnderstandingDelegateCall.sol
+++ src/proxy-pattern/UnderstandingDelegateCall.sol
@@ -1,18 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
-
-contract ImplContract {
-    uint256 slot1;
-    uint256 slot2;
-
-    function updateSlot1(uint256 _num) public {
-        slot1 = _num;        
-    }
-
-    function updateSlot2(uint256 _num) public {
-        slot2 = _num;        
-    }
-}
+import { ImplContract } from "./Implementation.sol";
 
 contract UnderstandingDelegateCall {
     uint256 public slot1;
